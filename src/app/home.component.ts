d6d94c1b48034b3ea4d4018b0b3ae12a617515f4
Dewanshu Sisaudiya <sisaudiya.dewan17@gmail.com>
2018-08-25 17:19:46 +0530

    changes

--- src/app/home.component.ts
+++ src/app/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {DomSanitizer} from '@angular/platform-browser';
 
 @Component({
   selector: 'app-home',
@@ -6,11 +7,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
+ workshops;
+  constructor(private sanitizer: DomSanitizer) { }
 
   ngOnInit() {
+    this.workshops = [
+    {cat:"Computer Science",
+     link:"/workshops/cs"
+    },
+      {cat:"Information Technology",
+        link:"/workshops/cs"},
+      {cat:"Electrical& Electronics",
+        link:"/workshops/ece"},
+      {cat:"Mechanical",
+        link:"/workshops/me"},
+      {cat:"Robotics",
+        link:"/workshops/robotics"},
+      {cat:"Civil",
+        link:"/workshops/civil"}
+    ]
 
   }
+  getBackground(image){
+    return this.sanitizer.bypassSecurityTrustUrl(image)
 
+  }
 }
