702c9036d9bf4d270574289dd8a739dc52999fed
Dewanshu Sisaudiya <sisaudiya.dewan17@gmail.com>
2018-08-31 00:12:18 +0530

    find

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,12 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home.component';
-import {RoutingModule} from './routing.module';
+import { RoutingModule } from './routing.module';
 import { HeaderComponent } from './common/header.component';
 import { SliderComponent } from './common/slider.component';
 import { FooterComponent } from './common/footer.component';
-import {FeaturesModule} from './features/features.module';
-import {TechfestModule} from './techfest/techfest.module';
+import { FeaturesModule } from './features/features.module';
+import { TechfestModule } from './techfest/techfest.module';
 
 
 @NgModule({
